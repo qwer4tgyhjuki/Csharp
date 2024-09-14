bb785e9cbed6481a75f9923d281cc30b19d3bf27
qwer4tgyhjuki <terokatop@gmail.com>
2024-09-14 17:22:47 +0300

    lr4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,92 +1,44 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
-public class Program
+﻿using System.Reflection;
+
+namespace ConsoleApp1
 {
-    /*
-     * Method reads file from current folder.
-     * User must enter integer number and then method gets the text up 
-     * to the charcater indexed  by entered number SPACE
-     * then make the substring and output the cut text
-     */
-    public void LoremIpsum()
+    class Program
     {
 
-        string text = File.ReadAllText("./lorem_ipsum.txt");
-        Console.Write("Enter count words: ");
-        string countWords = Console.ReadLine();
+        static void Main()
+        {
+            Mob slime = new Mob("Slime", 50, 0, 5, 5, MobState.Aggressive);
+            Mob ent = new Mob("Ent, Protector of the Forest", 200, 50, 60, 10, MobState.Netural);
 
-        if(!int.TryParse(countWords, out int count))
-        {
-            Console.WriteLine("Invalid number");
-            return;
-        }
-        if (count <= 0)
-        {
-            Console.WriteLine("Invalid number");
-            return;
-        }
-        int indexSpace = -1;
-        for(int i = 0; i < count; i++)
-        {
-            indexSpace = text.IndexOf(' ', indexSpace + 1);
-            if(indexSpace == -1)
+            Type slimeType = slime.GetType();
+            Console.WriteLine(slimeType);
+
+            TypeInfo entTypeInfo = ent.GetType().GetTypeInfo();
+            Console.WriteLine("\n" + entTypeInfo.Name);
+            Console.WriteLine(entTypeInfo.IsClass);
+
+
+            MemberInfo[] members = typeof(Mob).GetMembers();
+            foreach (MemberInfo member in members)
             {
-                return;
+                Console.WriteLine(member.Name);
             }
-        }
-        string selectedText = text.Substring(0, indexSpace);
-        Console.WriteLine(selectedText);
-    }
-    public void Calculator()
-    {
-        Console.WriteLine("Enter expression:");
-        /*
-         * Value type is string. Enter expression using numbers and operators and use
-         * method Parse for converting to expression
-         */
-        string expression = Console.ReadLine();
-        try
-        {            
-            double result = Parse(expression);
-            Console.WriteLine("Result: " + result);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-    }
-    double Parse(string expression)
-    {
-        try
-        {
-            return CSharpScript.EvaluateAsync<double>(expression).Result;
-        }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Error evaluating the expression.", ex);
-        }
-    }
-    public static void Main()
-    {
-        Program pr = new Program();
-        while (true)
-        {
-            Console.WriteLine("\nSelect action (0 - Exit, 1 - Lorem Ipsum 2 - Calculator): ");
-            char select = Console.ReadLine()[0];
-            switch (select)
+            Console.WriteLine("\n");
+            FieldInfo[] fields = typeof(Mob).GetFields();
+            FieldInfo privateField = typeof(Mob).GetField("mana", BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            foreach (FieldInfo field in fields)
             {
-                case '0':
-                    Console.WriteLine("Leaving");
-                    Environment.Exit(0);
-                    break;
-                case '1':
-                    pr.LoremIpsum();
-                    break;
-                case '2':
-                    pr.Calculator();
-                    break;
-                default:
-                    break;
+                Console.WriteLine(field.Name);
             }
+            Console.WriteLine($"Private filed: {privateField}");
+
+
+            MethodInfo decreaseMethodHPMethod = typeof(Mob).GetMethod("DecreaseHP");
+            object[] parameters = { 10 };
+            Console.WriteLine(ent);
+            decreaseMethodHPMethod.Invoke(ent, parameters);
+
         }
     }
 }
